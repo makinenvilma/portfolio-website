90fddc3e26c117d2f467305ae711e6c7f0b2302b
makinenvilma <vilmakinen@outlook.com>
2025-08-16 23:05:37 +0300

    Fix orb position

--- src/PortfolioSite.tsx
+++ src/PortfolioSite.tsx
@@ -92,7 +92,7 @@ function Hero() {
   return (
     <section className="relative isolate flex min-h-[86vh] items-center justify-center overflow-hidden px-6 pt-28">
       {/* Orb */}
-      <div className="absolute inset-0 -z-10 ">
+      <div className="absolute inset-0 -z-10 translate-y-10">
         <Orb
           hoverIntensity={0.5}
           rotateOnHover={true}
