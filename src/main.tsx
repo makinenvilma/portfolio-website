3e866bd63a637e62a054a8d2d82e817a9a7ac005
makinenvilma <vilmakinen@outlook.com>
2025-08-23 19:52:46 +0300

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import PortfolioSite from "./PortfolioSite";
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <PortfolioSite />
+    <Analytics />
   </React.StrictMode>
 );
